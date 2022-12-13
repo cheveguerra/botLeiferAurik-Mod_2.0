516d49eb62dca0b12fa929ea2bf7d774599d070d
cheveguerra <cheveguerra@gmail.com>
2022-12-12 22:57:38 -0600

    feat: cambio en mensaje inicial.

--- controllers/connection.js
+++ controllers/connection.js
@@ -1,7 +1,7 @@
 const connectionReady = (cb = () =>{}) => {
     console.log('Listo para escuchar mensajes')
     console.log('El cliente esta listo!');
-    console.log('🔴 Escribe: /hola');
+    console.log('🔴 Escribe: /menu');
     cb()
 }
 
